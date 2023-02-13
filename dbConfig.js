1c51f8f5c1b0c6fd1bad59c72292142cd517c000
johnkennady220 <115686295+johnkennady220@users.noreply.github.com>
2023-02-13 16:59:34 +0530

    web-scraping-backend

--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,6 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const dbName = 'webscrapping'
-const dbURL = `mongodb+srv:mongodb://localhost:27017/${dbName}`;
+const dbURL = `mongodb+srv://johnkennady220:HSL1c9dW7He94S9Z@cluster0.dw17smr.mongodb.net/${dbName}`;
 
 module.exports={dbURL,mongodb,MongoClient,dbName}
